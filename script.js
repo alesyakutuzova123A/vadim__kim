59e16ad80f8fa18c322dec64c0190bacd82b6553
windows <kutuzova@yandex.ru>
2021-02-22 17:18:01 +0300

    1

---
+++ script.js
@@ -0,0 +1,50 @@
+
+  $(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        slidesToShow:3,
+        autoplay: false,
+        speed:1000,
+        autoplaySpeed:1000,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow:2
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow:1
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function($) {
+	$('.popup-open').click(function() {
+		$('.popup-fade').fadeIn();
+		return false;
+	});	
+	
+	$('.popup-close').click(function() {
+		$(this).parents('.popup-fade').fadeOut();
+		return false;
+	});		
+ 
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade').fadeOut();
+		}
+	});
+	
+	$('.popup-fade').click(function(e) {
+		if ($(e.target).closest('.popup').length == 0) {
+			$(this).fadeOut();					
+		}
+	});
+});
